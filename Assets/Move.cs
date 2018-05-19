ef3dab44454bf0c9b41bac5a6604429d4d01311e
Adam Brigmann <a.brigmann@gmail.com>
2018-05-19 16:58:34 -0400

    Inital Commit for my project

---
+++ Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+	public Vector3 direction = Vector3.forward;
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Moves the cube to the right with a (1,0,0) vector
+		//transform.position += Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Time.deltaTime makes it run not based on frame rate
+		//How much to do every second
+		transform.position += transform.rotation*(direction.normalized*speed*Time.deltaTime);
+
+		//normalized makes it a one unit vector
+		//delta time == PER second
+	}
+}
---
+++ Assets/MoveLeftAndRight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftAndRight : MonoBehaviour 
+{
+	public float speed = 50.0f;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//input.GetAxis == gets a keyboard input for axis
+		transform.position += Vector3.right*Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+		//moves object left or right based on axis, speed, and time
+	}
+}
